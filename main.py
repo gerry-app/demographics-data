856b346c4f3b4acd9788737936ba35bec88c6853
Jason Kao <jasonkao85@gmail.com>
2018-01-21 07:18:35 -0500

    dl

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ with open('states.json') as f:
     states = ast.literal_eval(f.read())
     states_with_info = {}
     for key, value in states.items():
+        print key
         states_with_info[key] = d.get_state_people(key, states[key])
     with open('huge_ass.json', 'w') as g:
         json.dump(states_with_info, g)
