165789d79c2681e6206cf9e74ee39aefdc919e0e
Jason Kao <jasonkao85@gmail.com>
2018-01-21 03:52:19 -0500

    main

--- main.py
+++ main.py
@@ -6,6 +6,5 @@ with open('states.json') as f:
     states_with_info = {}
     for key, value in states.items():
         states_with_info[key] = d.get_state_people(key, states[key])
-    with open('hi.json', 'w') as g:
-        g.write(str(states_with_info["NY"]).replace("None", "null"))
-#        json.dump(states_with_info, g)
+    with open('huge_ass.json', 'w') as g:
+        json.dump(states_with_info, g)
